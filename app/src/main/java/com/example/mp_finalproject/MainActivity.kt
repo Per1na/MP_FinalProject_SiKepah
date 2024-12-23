f8330fdf03899a3f672a42d14e2cc5d6c47a877d
Dillon <dilonbonay@gmail.com>
2024-12-23 09:11:31 +0700

    layar buat akun pelanggan

--- app/src/main/java/com/example/mp_finalproject/MainActivity.kt
+++ app/src/main/java/com/example/mp_finalproject/MainActivity.kt
@@ -1,21 +1,62 @@
 package com.example.mp_finalproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.mp_finalproject.R.id.main
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContentView(R.layout.tampilan_awal)
+
+        setupBackButtonListeners()
+
+        val btnDriver: Button = findViewById(R.id.driverButton)
+        btnDriver.setOnClickListener {
+            setContentView(R.layout.buat_akun_driver)
+
+            val backButtonDriver: ImageView = findViewById(R.id.back_button)
+            backButtonDriver.setOnClickListener {
+                setContentView(R.layout.tampilan_awal)
+                setupBackButtonListeners()
+            }
+        }
+
+        val btnPelanggan: Button = findViewById(R.id.pelangganButton)
+        btnPelanggan.setOnClickListener {
+            setContentView(R.layout.buat_akun_pelanggan)
+
+            val backButtonPelanggan: ImageView = findViewById(R.id.back_button)
+            backButtonPelanggan.setOnClickListener {
+                setContentView(R.layout.tampilan_awal)
+                setupBackButtonListeners()
+            }
+        }
+    }
+
+    private fun setupBackButtonListeners() {
+        val btnDriver: Button = findViewById(R.id.driverButton)
+        btnDriver.setOnClickListener {
+            setContentView(R.layout.buat_akun_driver)
+
+            val backButtonDriver: ImageView = findViewById(R.id.back_button)
+            backButtonDriver.setOnClickListener {
+                setContentView(R.layout.tampilan_awal)
+                setupBackButtonListeners()
+            }
+        }
+
+        val btnPelanggan: Button = findViewById(R.id.pelangganButton)
+        btnPelanggan.setOnClickListener {
+            setContentView(R.layout.buat_akun_pelanggan)
+
+            val backButtonPelanggan: ImageView = findViewById(R.id.back_button)
+            backButtonPelanggan.setOnClickListener {
+                setContentView(R.layout.tampilan_awal)
+                setupBackButtonListeners()
+            }
         }
     }
 }
